d05e49be0ec68b0d07d5e716ceec86b1a4ad43ba
s3943811 <s3943811@student.rmit.edu.au>
2023-07-15 22:19:39 +1000

    Setup for Project 1

---
+++ Project1-StormViewer/StormViewer/StormViewer/StormViewerApp.swift
@@ -0,0 +1,17 @@
+//
+//  StormViewerApp.swift
+//  StormViewer
+//
+//  Created by Maximus Dionyssopoulos on 15/7/2023.
+//
+
+import SwiftUI
+
+@main
+struct StormViewerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
