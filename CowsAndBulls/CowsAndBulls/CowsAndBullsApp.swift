870642832d640ac877b77f8c8c0bf5e7cbd7c35e
s3943811 <s3943811@student.rmit.edu.au>
2023-07-16 16:36:25 +1000

    Project 2 setup

---
+++ CowsAndBulls/CowsAndBulls/CowsAndBullsApp.swift
@@ -0,0 +1,17 @@
+//
+//  CowsAndBullsApp.swift
+//  CowsAndBulls
+//
+//  Created by Maximus Dionyssopoulos on 16/7/2023.
+//
+
+import SwiftUI
+
+@main
+struct CowsAndBullsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
