bc9524848c6f585096ecfeeee8e892042c765184
s3943811 <s3943811@student.rmit.edu.au>
2023-07-16 19:42:58 +1000

    Setup for project 3

---
+++ ViewsAndModifiers/ViewsAndModifiers/ViewsAndModifiersApp.swift
@@ -0,0 +1,17 @@
+//
+//  ViewsAndModifiersApp.swift
+//  ViewsAndModifiers
+//
+//  Created by Maximus Dionyssopoulos on 16/7/2023.
+//
+
+import SwiftUI
+
+@main
+struct ViewsAndModifiersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
