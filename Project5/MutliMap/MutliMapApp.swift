09a8d77118cf838c6de1df4c279ca58d7324a161
s3943811 <s3943811@student.rmit.edu.au>
2023-07-19 17:55:16 +1000

    Setup for Project 5: MultiMap

---
+++ Project5/MutliMap/MutliMapApp.swift
@@ -0,0 +1,17 @@
+//
+//  MutliMapApp.swift
+//  MutliMap
+//
+//  Created by Maximus Dionyssopoulos on 19/7/2023.
+//
+
+import SwiftUI
+
+@main
+struct MutliMapApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
