ce4de8c7370a3f1df83ddffb4e044f5919ab8937
s3943811 <s3943811@student.rmit.edu.au>
2023-07-22 16:43:18 +1000

    Project 8 Setup

---
+++ Project8/OddOneOut/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  OddOneOut
+//
+//  Created by Maximus Dionyssopoulos on 22/7/2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Project8/OddOneOut/OddOneOutApp.swift
@@ -0,0 +1,17 @@
+//
+//  OddOneOutApp.swift
+//  OddOneOut
+//
+//  Created by Maximus Dionyssopoulos on 22/7/2023.
+//
+
+import SwiftUI
+
+@main
+struct OddOneOutApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
