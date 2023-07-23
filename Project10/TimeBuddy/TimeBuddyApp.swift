ed661b459175158853dfccc901d8c75f68559080
s3943811 <s3943811@student.rmit.edu.au>
2023-07-23 16:52:16 +1000

    Project 10 Setup
    Decided to do project 10 before 9, will revisit 9 next

---
+++ Project10/TimeBuddy/TimeBuddyApp.swift
@@ -0,0 +1,17 @@
+//
+//  TimeBuddyApp.swift
+//  TimeBuddy
+//
+//  Created by Maximus Dionyssopoulos on 23/7/2023.
+//
+
+import SwiftUI
+
+@main
+struct TimeBuddyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
